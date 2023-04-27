f415f48db55fa5591fa70734d8fc53581cafedc3
Murilothebr <murilothebr@hotmail.com>
2023-04-27 09:54:59 -0300

    go maps / dictionary

--- hello.go
+++ hello.go
@@ -3,13 +3,34 @@ package main
 import "fmt"
 
 func main() {
-	a := []int{}
-	b := make([]int, 1)
 
-	fmt.Println(a)
-	fmt.Println(b)
+	m := make(map[string]int)
 
-	a = append(a, 1)
+	m["k1"] = 7
+	m["k2"] = 10 
 
-	fmt.Println(a)
-}
+	fmt.Println("map:", m)
+
+	v1 := m["k1"]
+
+	fmt.Println(v1)
+
+	v3 := m["k3"]
+
+	fmt.Println(v3)
+
+	fmt.Println(len(m))
+
+	delete(m, "k2")
+
+	fmt.Println(m)
+
+	_, prs := m["k1"]
+    fmt.Println("prs:", prs)	
+
+	n := map[int]string{1: "a", 2: "b"}
+    fmt.Println(n)	
+
+
+
+}	
