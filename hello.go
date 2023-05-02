f2ddc37e24e8175e7b4e4ce7bc023286e60a6ff3
Murilothebr <murilothebr@hotmail.com>
2023-05-02 11:22:10 -0300

    first func

--- hello.go
+++ hello.go
@@ -2,35 +2,12 @@ package main
 
 import "fmt"
 
+func soma(a int, b int) int	{
+	return a + b
+}
+
 func main() {
 
-	m := make(map[string]int)
 
-	m["k1"] = 7
-	m["k2"] = 10 
-
-	fmt.Println("map:", m)
-
-	v1 := m["k1"]
-
-	fmt.Println(v1)
-
-	v3 := m["k3"]
-
-	fmt.Println(v3)
-
-	fmt.Println(len(m))
-
-	delete(m, "k2")
-
-	fmt.Println(m)
-
-	_, prs := m["k1"]
-    fmt.Println("prs:", prs)	
-
-	n := map[int]string{1: "a", 2: "b"}
-    fmt.Println(n)	
-
-
-
-}	
+	fmt.Println(soma(10, 1))
+}
